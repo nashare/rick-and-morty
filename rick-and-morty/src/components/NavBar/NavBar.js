c56cfc14a2ef2f7ae1982cc5ebf830ff36969027
nashare <tatiana.lavr@gmail.com>
2023-05-15 12:30:51 -0700

    navbar css

--- rick-and-morty/src/components/NavBar/NavBar.js
+++ rick-and-morty/src/components/NavBar/NavBar.js
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <nav>
-      <Link to="/">CHARACTERS</Link>
-      <Link to="/locations">LOCATIONS</Link>
-      <Link to="/episodes">EPISODES</Link>
+      <Link to="/" className="nav-link">CHARACTERS</Link>
+      <Link to="/locations" className="nav-link">LOCATIONS</Link>
+      <Link to="/episodes" className="nav-link">EPISODES</Link>
     </nav>
   );
 }
