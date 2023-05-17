27e50cc2e8645874334b0d38faf8785f0f874281
nashare <tatiana.lavr@gmail.com>
2023-05-16 22:30:03 -0700

    pagination range

--- rick-and-morty/src/components/Pagination/Pagination.js
+++ rick-and-morty/src/components/Pagination/Pagination.js
@@ -1,6 +1,49 @@
 import './Pagination.css';
+import { useMemo } from 'react';
 
 function Pagination({ info, page, setPage }) {
+
+  const paginationRange = useMemo(() => {
+    const totalPageNumbers = 7;
+    const siblingCount = 1;
+
+    function rangeArray (start, end) {
+      return Array.from(Array(end - start + 1).keys(), (num) => num + start);
+    }
+    if (totalPageNumbers >= info.pages) {
+      return rangeArray(1, info.pages);
+    }
+    const leftSiblingIndex = Math.max(page - siblingCount, 1);
+    const rightSiblingIndex = Math.min(
+      page + siblingCount,
+      info.pages
+    );
+    const shouldShowLeftDots = leftSiblingIndex > 2;
+    const shouldShowRightDots = rightSiblingIndex < info.pages - 2;
+
+    const firstPageIndex = 1;
+    const lastPageIndex = info.pages;
+    if (!shouldShowLeftDots && shouldShowRightDots) {
+      let leftItemCount = 3 + 2 * siblingCount;
+      let leftRange = rangeArray(1, leftItemCount);
+      return [...leftRange, "DOTS", info.pages];
+    }
+    if (shouldShowLeftDots && !shouldShowRightDots) {
+      let rightItemCount = 3 + 2 * siblingCount;
+      let rightRange = rangeArray(
+        info.pages - rightItemCount + 1,
+        info.pages
+      );
+      return [firstPageIndex, "DOTS", ...rightRange];
+    }
+    if (shouldShowLeftDots && shouldShowRightDots) {
+      let middleRange = rangeArray(leftSiblingIndex, rightSiblingIndex);
+      return [firstPageIndex, "DOTS", ...middleRange, "DOTS", lastPageIndex];
+    }
+  }, [info, page]);
+
+  console.log(paginationRange);
+
   const buttons = [];
   for (let i = 1; i <= info.pages; i++) {
     const isCurrentPage = i === page;
